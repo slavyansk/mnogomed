d32e33f485caa49f1c8486453acc141e610fdff0
Ivan_Ligachev <slavyansk2001@mail.ru>
2021-02-28 19:37:03 +0300

    исправление файла 1.py

--- 1.py
+++ 1.py
@@ -1,10 +1,8 @@
 import requests
 from datetime import date, datetime
 import sqlite3
+from db import Database
 
-conn = sqlite3.connect("mydatabase.db")
-print('БД ок!')
-cursor = conn.cursor()
 
 #настройка сервера и даты
 server = 'http://10.53.20.15:8042'
@@ -26,14 +24,6 @@ def reverse_date(date):
         dateofbirth_raw = datetime.strptime(date, "%Y%m%d")
         return dateofbirth_raw.strftime('%d.%m.%Y')
 
-def create_table():
-    # Создание таблицы
-    cursor.execute("""CREATE TABLE IF NOT EXISTS list_of_patients
-                  (studydate TEXT, name TEXT, birthday TEXT, p_sex TEXT,
-                  body_part TEXT, SeriesDescription TEXT, report TEXT)
-                   """)
-    # Сохраняем изменения
-    conn.commit()
 
 def orthanc_connect(server,today):
     data = '{ "Level" : "Instance", "Query" : { "StudyDate" : "'+ today + '" } }'
@@ -62,26 +52,22 @@ else:
         else:
             p_sex = 'M'
         studydate = reverse_date(p['StudyDate']+p['StudyTime'])
-        body_part = p["BodyPartExamined"]
-        seriesdescription = p['SeriesDescription']
-        report = input(name+' '+birthday+' '+ body_part+' Ведите описание:') #вставить в другое место
+        examination = p["BodyPartExamined"] + p['SeriesDescription']
+        report = '-'
 
-        user_study = (studydate, name, birthday, p_sex,
-                      body_part, seriesdescription, report)
+        user_study = (studydate, name, birthday, p_sex, examination, report)
 
-        create_table()
+        db = Database('my_db.db')
 
-        cursor.execute("""SELECT studydate, name, birthday FROM list_of_patients WHERE
+        db.cur.execute("""SELECT studydate, name, birthday FROM list_of_patients WHERE
                        (studydate LIKE ? AND name = ? AND birthday = ?);""", ('%'+studydate[0:10]+'%', name, birthday))
-        search = cursor.fetchone()
+        search = db.cur.fetchone()
 
         if search is None:
-            cursor.execute("""INSERT INTO list_of_patients VALUES(?,?,?,?,?,?,?);
-                       """,user_study)
-            conn.commit()
+            db.insert(user_study)
 
-cursor.execute("SELECT * FROM list_of_patients WHERE report = '-'")
-all_results = cursor.fetchall()
+db.cur.execute("SELECT * FROM list_of_patients WHERE report = '-'")
+all_results = db.cur.fetchall()
 for row in all_results:
     for c in row:
         print(c, end=" ")
